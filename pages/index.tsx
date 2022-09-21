cff7d6c485361bbc9107de600c32b3a3919fda60
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-09-21 22:31:14 +0530

    fix: svg fill error

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,7 +149,6 @@ const Home: NextPage = () => {
 							<svg
 								className="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
 								viewBox="0 0 100 101"
-								fill="none"
 								xmlns="http://www.w3.org/2000/svg"
 							>
 								<path
