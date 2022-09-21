0e31518e21710023faadf413189bf5fd60b977aa
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-09-22 00:02:49 +0530

    fixes

--- pages/components/Modal.tsx
+++ pages/components/Modal.tsx
@@ -41,29 +41,9 @@ function Modal({ set, cid }: any) {
 										className="w-full py-4 pl-3 pr-16 text-sm border-2 border-gray-200 rounded-lg"
 										id="email"
 										placeholder="Link"
-										value={`https://wagmi-web/agreement/${cid}`}
+										value={`https://wagmi-web.vercel.app/agreement/${cid}`}
 										readOnly={true}
 									/>
-
-									<button
-										className="absolute p-2 text-white bg-blue-600 rounded-full -translate-y-1/2 top-1/2 right-4"
-										type="button"
-									>
-										<svg
-											className="w-4 h-4"
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												strokeWidth="2"
-												d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-											/>
-										</svg>
-									</button>
 								</div>
 							</div>
 
